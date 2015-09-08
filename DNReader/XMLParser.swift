b637a2a49945b25e03c5377fc6777172977f0e33
Felix Hedlund <felixhedlund@me.com>
2015-09-08 10:43:02 +0200

    XMLParser ready

--- DNReader/XMLParser.swift
+++ DNReader/XMLParser.swift
@@ -8,6 +8,52 @@
 
 import UIKit
 
-class XMLParser: NSObject {
-   
+@objc protocol XMLParserDelegate{
+    func parsingWasFinished()
+}
+
+class XMLParser: NSObject, NSXMLParserDelegate {
+    
+    var arrParsedData = [Dictionary<String, String>]()
+    var currentDataDictionary = Dictionary<String, String>()
+    var currentElement = ""
+    var foundCharacters = ""
+    
+    var delegate : XMLParserDelegate?
+    
+    func startParsingWithContentsOfURL(rssURL: NSURL) {
+        let parser = NSXMLParser(contentsOfURL: rssURL)!
+        parser.delegate = self
+        parser.parse()
+    }
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if !foundCharacters.isEmpty {
+            
+            if elementName == "link"{
+                foundCharacters = (foundCharacters as NSString).substringFromIndex(3)
+            }
+            
+            currentDataDictionary[currentElement] = foundCharacters
+            
+            foundCharacters = ""
+            
+            if currentElement == "pubDate" {
+                arrParsedData.append(currentDataDictionary)
+            }
+        }
+    }
+    
+    func parserDidEndDocument(parser: NSXMLParser) {
+        delegate?.parsingWasFinished()
+    }
+    
+    func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+        println(parseError.description)
+    }
+    
+    
+    func parser(parser: NSXMLParser, validationErrorOccurred validationError: NSError) {
+        println(validationError.description)
+    }
 }
